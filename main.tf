3a4f657fe4072cbaab2c4e03f25b101c45c1d56b
Edgar Lange <edgar_lange@outlook.com>
2023-08-18 01:15:34 -0500

    Practica de modulos

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 module "olympo_bucket" {
   source      = "./modules/s3"
-  bucket_name = "olympo-ghjkl"
+  bucket_name = "olympo-ghjklww"
 }
-
 
 output "s3_arn" {
   value = module.olympo_bucket.s3_bucket_arn
