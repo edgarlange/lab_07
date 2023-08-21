b62d6dc8bb4bdeae0bd33fd0f9b59e55508881ec
Edgar Lange <edgar_lange@outlook.com>
2023-08-20 22:36:05 -0500

    Ajustes post creacipn modulo VPC

--- ec2.tf
+++ ec2.tf
@@ -3,9 +3,9 @@ resource "aws_instance" "public_ec2" {
   for_each               = toset(var.instancias)
   ami                    = var.ec2_spec.ami
   instance_type          = var.ec2_spec.instance_type
-  subnet_id              = module.olympo_vpc.public_subnet[count.index] # aws_subnet.public_subnet.id
+  subnet_id              = module.olympo_vpc.public_subnet[0] # aws_subnet.public_subnet.id
   key_name               = data.aws_key_pair.lange2_key.key_name
-  vpc_security_group_ids = [module.olympo_vpc.aws_security_group.sg_public_instance.id] # [aws_security_group.sg_public_instance.id]
+  vpc_security_group_ids = [module.olympo_vpc.sg_public_instance[0]] # [aws_security_group.sg_public_instance.id]
   user_data              = file("scripts/userdata.sh")
 
   tags = {
@@ -17,9 +17,9 @@ resource "aws_instance" "monitoring_ec2" {
   count                  = var.enable_monitoring == 1 ? 1 : 0
   ami                    = var.ec2_spec.ami
   instance_type          = var.ec2_spec.instance_type
-  subnet_id              = module.olympo_vpc.public_subnet[count.index]
+  subnet_id              = module.olympo_vpc.public_subnet[0]
   key_name               = data.aws_key_pair.lange2_key.key_name
-  vpc_security_group_ids = [module.olympo_vpc.aws_security_group.sg_public_instance.id]
+  vpc_security_group_ids = [module.olympo_vpc.sg_public_instance[0]]
   user_data              = file("scripts/userdata.sh")
 
   tags = {
