a4b638ac83fc7adb4b961092af53801bd209ff9c
Edgar Lange <edgar_lange@outlook.com>
2023-08-20 22:57:03 -0500

    Ajustes post creacipn modulo VPC

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "public_ec2" {
   for_each               = toset(var.instancias)
   ami                    = var.ec2_spec.ami
   instance_type          = var.ec2_spec.instance_type
-  subnet_id              = module.olympo_vpc.public_subnet[0] # aws_subnet.public_subnet.id
+  subnet_id              = module.olympo_vpc.public_subnet
   key_name               = data.aws_key_pair.lange2_key.key_name
   vpc_security_group_ids = [module.olympo_vpc.sg_public_instance[0]] # [aws_security_group.sg_public_instance.id]
   user_data              = file("scripts/userdata.sh")
@@ -17,9 +17,9 @@ resource "aws_instance" "monitoring_ec2" {
   count                  = var.enable_monitoring == 1 ? 1 : 0
   ami                    = var.ec2_spec.ami
   instance_type          = var.ec2_spec.instance_type
-  subnet_id              = module.olympo_vpc.public_subnet[0]
+  subnet_id              = module.olympo_vpc.public_subnet
   key_name               = data.aws_key_pair.lange2_key.key_name
-  vpc_security_group_ids = [module.olympo_vpc.sg_public_instance[0]]
+  vpc_security_group_ids = [module.olympo_vpc.sg_public_instance]
   user_data              = file("scripts/userdata.sh")
 
   tags = {
